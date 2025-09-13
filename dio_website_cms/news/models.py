442762deb24a4c6bceaa9e7019fa6f64821c8b9d
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-09-13 12:55:20 +0500

    13.09.2025 Добавлены новые страницы

--- dio_website_cms/news/models.py
+++ dio_website_cms/news/models.py
@@ -5,4 +5,4 @@ from wagtail.models import Page
 class NewsPage(Page):
     """Новостная страница"""
     publish_date = models.DateField(auto_now_add=True, verbose_name="Дата публикации")
-    headline = models.CharField(blank=True, verbose_name="Новостной заголовок ")
+    headline = models.CharField(blank=True, verbose_name="Новостной заголовок")
