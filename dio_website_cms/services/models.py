267f8da4872b9d64eba5ae0617b2030fd906568b
DreminIlarion <anasgodvaan@gmail.com>
2025-11-27 22:28:26 +0500

    27.11.2025 Добавил изменения, которые менял в контейнере

--- dio_website_cms/services/models.py
+++ dio_website_cms/services/models.py
@@ -311,3 +311,15 @@ class ServiceBlock(blocks.StructBlock):
     class Meta:
         icon = "doc-full"
         label = "Блок услуг"
+
+class ServicePage(Page):
+    """Временная модель для обратной совместимости"""
+    body = RichTextField(blank=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+    
+    class Meta:
+        verbose_name = "Страница услугиbb"
+        verbose_name_plural = "Страницы услугbb"
